c0d1ddde47b0c59b913b9dcf38ce00cf017641ef
s6652410017 <142290966+s6652410017@users.noreply.github.com>
2023-12-06 19:01:46 +0700

    Delete dtitest06.py

--- dtitest06.py
+++
@@ -1,8 +0,0 @@
-ฃื่อ_product = input("ใส่ฃื่อ product")
-budget = float(input("ใส่ budget product"))
-
-def productVAlue(budget):
-    product_ราคาจริง = budget + (budget * 10/100)
-    return "%.2f" % product_ราคาจริง
-
-print(f"ราคาของ {ฃื่อ_product} คือ {productVAlue(budget)}")
